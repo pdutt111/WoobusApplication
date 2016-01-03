f41be06c441f9d51393316a3ce6d44f1ed5ef3ba
Rohit Gohri <gohri.rohit@gmail.com>
2016-01-04 01:14:53 +0530

    journey modals

--- www/js/custom-journey.js
+++ www/js/custom-journey.js
@@ -27,40 +27,49 @@ function getRouteDetails() {
 function dataParserRoute(data) {
     var journeyData= JSON.stringify(data);
     var data=JSON.parse(journeyData);
+
     console.log(data.start);
     $("#from").html(data.start);
     $("#to").html(data.end);
+
     $("#distance").html(data.current_distance+" Kms");
     console.log(data.distance);
+
     var position_marker=((data.distance-data.current_distance)/data.distance)*78;//78 is because of the margins and so that it stays in the bounds needs to be sorted
+
     $("#marker").css({'left':position_marker+6+'%'});
+
     console.log(data.current_time_taken);
     var time_taken = (data.current_time_taken/(1000*60*60)).toFixed(2)+" Hrs";
     $("#time").html(time_taken);
+
     console.log(journeyData);
 
-    for(var i=0;i<data.scheduled_stops.length;i++){
+    for(var i=0; i < data.scheduled_stops.length; i++){
         //console.log()
-        $("#stops").append('<a data-toggle="modal" data-target="#myModal" onclick="dialog('+i+')" href="#">' +
-            '<i class="fa fa fa-cutlery position_show_main1" data-toggle="tooltip" data-placement="top"' +
-            ' title="Restaurants"></i></a>');
+        $("#stops").append('<a class="bin tooltipped modal-trigger" data-position="top" data-delay="50" data-tooltip="Restaurants" onclick="dialog(' +
+            i + ')" href="#modal2">' +
+            '<i class="material-icons white-text">restaurant_menu</i></a>');
     }
     sessionStorage.setItem('journey', journeyData);
     //window.location.href = "profile.html";
 }
+
 function dialog(count){
-    var journey=JSON.parse(sessionStorage.getItem("journey"));
+    var journey = JSON.parse(sessionStorage.getItem("journey"));
     console.log(journey);
-    var dialog=journey.scheduled_stops[count];
+
+    var dialog = journey.scheduled_stops[count];
+
     $("#stop_title").html(dialog.name);
-    for(var i=0;i<dialog.restaurants_available.length;i++){
+    for(var i=0; i < dialog.restaurants_available.length; i++){
         $("#stop_restaurants").append(dialog.restaurants_available[i]+"<br>");
     }
     console.log(dialog.is_loo);
-    if(dialog.is_loo){
+    if (dialog.is_loo) {
         $("#stop_facilities").append('<img class="facility-icon" src="images/icons-png/loo.png">')
     }
-    if(dialog.is_food){
+    if (dialog.is_food) {
         $("#stop_facilities").append('<img class="facility-icon" src="images/icons-png/food.png">')
     }
 }
